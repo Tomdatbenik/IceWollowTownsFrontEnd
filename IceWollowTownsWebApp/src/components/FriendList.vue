ca47164236a9939ff6d579d7d33597fef945201a
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-06 11:53:51 +0100

    Friend api call with correct vue instalation

---
+++ IceWollowTownsWebApp/src/components/FriendList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="friend">
+    <h1>{{ friend }}</h1>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'FriendList',
+  props: {
+    friend: null
+  },
+    mounted () {
+    axios
+      .get('http://localhost:8080/friend',{ params:{
+          id: '0'
+      }})
+      .then(response => {
+        this.friend = response.data.username
+      })
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
