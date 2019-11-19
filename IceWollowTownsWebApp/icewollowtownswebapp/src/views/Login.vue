ad1e0d56cb9ded361ac4b8ca356f555d85d054d9
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-19 13:20:24 +0100

    Login page

---
+++ IceWollowTownsWebApp/icewollowtownswebapp/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="row">
+    <div class="col-6 offset-3">
+      <div class="card rounded">
+        <div class="card-header">
+          <div class="card-title">
+            <h4 class="display-4">IceWollowTowns</h4>
+          </div>
+        </div>
+        <div class="card-body">
+          <div v-if="!$auth.loading">
+            <!-- show login when not authenticated -->
+            <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+            <!-- show logout when authenticated -->
+            <div v-if="$auth.isAuthenticated">
+              <h2>{{ $auth.user.name }}</h2>
+              <img :src="$auth.user.picture" />
+              <button @click="logout">Log out</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "login",
+  components: {},
+  mounted: function() {
+    if (this.$auth.isAuthenticated) {
+      this.Router.push("/home");
+    }
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+};
+</script>
